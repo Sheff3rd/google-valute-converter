66ed60162b81160b09b5bf27b8bd10e45ec2f1b6
sheff3rd <thedoctorwhoovesmail@gmail.com>
2018-02-19 13:26:20 +0200

    initial commit

---
+++ converter.rb
@@ -0,0 +1,37 @@
+require 'net/http'
+
+class Converter
+  class Currency
+    def initialize(currency, amout)
+      @value = (currency.to_f * amout).round(2)
+      @course = currency.to_f
+      @currency = /[a-zA-Z]+/.match(currency)[0]
+    end
+  end
+
+  def initialize(from, to, amout = 1)
+    @from = from
+    @to = to
+    @amout = amout
+  end
+
+  def convert
+    span = /<span class=bld>(.*)<\/span>/.match(response).captures[0]
+    Currency.new(span, @amout)
+  end
+
+  private
+
+  def response
+    Net::HTTP.get(uri)
+      .encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+  end
+
+  def url
+    "https://finance.google.com/finance/converter?a=1&from=#{@from}&to=#{@to}"
+  end
+
+  def uri
+    URI(url)
+  end
+end
